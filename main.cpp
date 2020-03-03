5208035a3256b53b7bd900219b0e8a4583a8eafe
Mark Angelo Capili <201910375@fit.edu.ph>
2020-03-03 22:20:47 +0800

    fixed login bug

--- main.cpp
+++ main.cpp
@@ -4,52 +4,37 @@
 using namespace std;
 string admin;
 string pass;
-bool login = false;
 
 
-void adminLogin();
+bool login(const string &username, const string &password);
 
 int main(){ // main function
 	
-	adminLogin();
-	if (login){
-		cout << "WELCOME ADMIN " << admin;
-	}
-
-}
-
-
-void adminLogin(){ //admin log-in system
 	cout << "User Name: ";
 	cin >> admin;
 	cout << "Password: ";
 	cin >> pass;
-	
-	string userPass = admin + " " + pass; //this line will be searched
-	int offset;
-	string line;
-	ifstream adminData;
-	adminData.open("adminData.txt");
-	
-	if(adminData.is_open()){
-		while (!adminData.eof()){
-			getline(adminData,line);
-			if ((offset = line.find(pass, 0)) != string::npos) // if log in data is found 
-			{
-				login = 1;
-			}\
-			else // if log in data !found 
-			{
-				cout << "\n INCORRECT ADMIN CREDENTIALS.";
-			}
-		}
-		
-		adminData.close();
+	bool loginpwe = login(admin,pass);
+	if(loginpwe){
+		cout << "WELCOME " << admin;
+	} else {
+		cout << "INVALID USER CREDENTIALS";
 	}
-	else
-		cout << "ERROR: Unable to open admin database!. ";
-	
-	
+}
+
+
+bool login(const string &username, const string &password) {
+    ifstream file("adminData.txt");
+    std::string fusername, fpassword;
+
+    while (file) {
+        getline(file, fusername, ';'); 
+        getline(file, fpassword); 
+ 
+        if (fusername == username && fpassword == password)
+            return true;
+    }
+    return false;
 }
 
 
