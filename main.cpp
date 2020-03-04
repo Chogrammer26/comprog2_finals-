46359515d4fd7d41598ac048bbc8acbfc693a740
Mark Angelo Capili <201910375@fit.edu.ph>
2020-03-04 15:58:03 +0800

    added menu and gotoxy functions

--- main.cpp
+++ main.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
 #include <conio.h>
+#include <windows.h>
 #include <fstream> //for IO of file
 using namespace std;
+
+// GLOBAL VARIABLES
 string admin;
 string pass;
+int choice;
 
-
+//FUNCTIONS
+void menu();
+void PlaceCursor(const int x, const int y); // in case we want to position every text
 bool login(const string &username, const string &password);
 
 int main(){ // main function
@@ -16,7 +22,10 @@ int main(){ // main function
 	cin >> pass;
 	bool loginpwe = login(admin,pass);
 	if(loginpwe){
-		cout << "WELCOME " << admin;
+		cout << "WELCOME " << admin << endl;
+		system("pause");
+		system("cls");
+		menu();
 	} else {
 		cout << "INVALID USER CREDENTIALS";
 	}
@@ -37,4 +46,25 @@ bool login(const string &username, const string &password) {
     return false;
 }
 
+void menu(){
+	cout << "1. MAKE RESERVATION\n";
+	cout << "2. CANCEL RESERVATION\n";
+	cout << "3. SEARCH PASSENGER\n";
+	cout << "4. CHANGE RESERVATION\n";
+	cout << "5. PRINT RECEIPT\n";
+	cout << "QUIT\n";
+	cin >> choice;
+}
+
+void PlaceCursor(const int x, const int y) {
+
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+
+    COORD PlaceCursorHere;
+    PlaceCursorHere.X = x;
+    PlaceCursorHere.Y = y;
+
+    SetConsoleCursorPosition(hConsole, PlaceCursorHere);
+    return;
+}
 
