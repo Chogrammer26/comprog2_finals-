41c1674fa17f2ae2f31b58f13c60ec3c84829249
Mark Capili <angelocapili44@gmail.com>
2020-03-07 08:33:47 +0800

    added logo

--- main.cpp
+++ main.cpp
@@ -13,9 +13,11 @@ int choice;
 void menu();
 void PlaceCursor(const int x, const int y); // in case we want to position every text
 bool login(const string &username, const string &password);
+void logo();
 
 int main(){ // main function
-	
+	logo();
+	system("pause");
 	cout << "User Name: ";
 	cin >> admin;
 	cout << "Password: ";
@@ -75,7 +77,15 @@ void PlaceCursor(const int x, const int y) {
 }
 
 
-
+void logo() {
+	cout << "______  ___             ______             ____________                                        \n";
+	cout << "___   |/  /___  ___________  /_________    ___    |__(_)_________      _______ _____  _________\n";
+	cout << "__  /|_/ /_  / / /_  ___/_  //_/_  ___/    __  /| |_  /__  ___/_ | /| / /  __ `/_  / / /_  ___/\n";
+	cout << "_  /  / / / /_/ /_(__  )_  ,<  _(__  )     _  ___ |  / _  /   __ |/ |/ // /_/ /_  /_/ /_(__  )\n";
+	cout << "/_/  /_/  \\__,_/ /____/ /_/|_| /____/      /_/  |_/_/  /_/    ____/|__/ \\__,_/ _\\__, / /____/\n";
+	cout << "                                                                               /____/        \n";
+	
+}
 
 
 
